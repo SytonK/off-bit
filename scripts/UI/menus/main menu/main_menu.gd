893f67349083ba50ab15a552e20345fecc708e0e
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-27 22:44:20 +0200

    feat/add menu selection

---
+++ scripts/UI/menus/main menu/main_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var back_button: Button = $BackButton
+@onready var mode_selector: VBoxContainer = $MenuSwapper/ModeSelector
+@onready var base_mode_menu: VBoxContainer = $MenuSwapper/BaseModeMenu
+@onready var advanced_mode_menu: VBoxContainer = $MenuSwapper/AdvancedModeMenu
+
+var current_menu: Control
+
+func _change_to_menu(menu: Control) -> void:
+	mode_selector.visible = false
+	back_button.visible = true
+	menu.visible = true
+	current_menu = menu
+
+func _back_from_menu(menu: Control) -> void:
+	mode_selector.visible = true
+	back_button.visible = false
+	menu.visible = false
+
+func _on_back_button_pressed() -> void:
+	_back_from_menu(current_menu)
+
+func _on_base_mode_button_pressed() -> void:
+	_change_to_menu(base_mode_menu)
+
+func _on_advanced_mode_button_pressed() -> void:
+	_change_to_menu(advanced_mode_menu)
