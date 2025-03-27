0f082d7cb7837a61f5d87b2f2d20ac2c7fae4048
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-27 15:18:01 +0200

    feat/cell press detection

--- scripts/cell/cell.gd
+++ scripts/cell/cell.gd
@@ -1,7 +1,8 @@
 class_name Cell extends Node2D
 
-@onready var label: Label = $Label
+signal cell_pressed(index: Vector2i)
 
+@onready var label: Label = $Label
 
 static var modulo: int = 2;
 
@@ -12,3 +13,10 @@ var value: int = 0: set = set_value;
 func set_value(new_value: int):
 	value = posmod(new_value, modulo)
 	label.text = str(value)
+
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("pressed"):
+		cell_pressed.emit(index)
+	elif event is InputEventScreenTouch and event.is_pressed():
+		cell_pressed.emit(index)
