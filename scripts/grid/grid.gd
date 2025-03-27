cef9042889377cf27a1ade42cd2e42af11d45f76
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-27 19:59:08 +0200

    feat/detect win condition

--- scripts/grid/grid.gd
+++ scripts/grid/grid.gd
@@ -2,11 +2,14 @@ class_name Grid extends Node2D
 
 const CELL = preload("res://scenes/cell/cell.tscn")
 
+signal win
+
 @export var height: int = 3
 @export var width: int = 3
 @export var modulo: int = 2
 
 var _cells: Array[Array] #Array[Array[Cell]]
+var num_of_on_bits: int = 0: set = _set_num_of_on_bits
 
 func _ready() -> void: 
 	Cell.modulo = modulo
@@ -37,15 +40,15 @@ func _init_grid() -> void:
 func _on_cell_pressed(index: Vector2i) -> void:
 	assert(index.x >= 0 && index.x < width && index.y >= 0 && index.y < height)
 	
-	_cells[index.x][index.y].value -= 1
+	_cycle(index)
 	if index.x > 0:
-		_cells[index.x - 1][index.y].value -= 1
+		_cycle(index + Vector2i(-1,0))
 	if index.x < width - 1:
-		_cells[index.x + 1][index.y].value -= 1
+		_cycle(index + Vector2i(1,0))
 	if index.y > 0:
-		_cells[index.x][index.y - 1].value -= 1
+		_cycle(index + Vector2i(0,-1))
 	if index.y < height - 1:
-		_cells[index.x][index.y + 1].value -= 1
+		_cycle(index + Vector2i(0,1))
 
 func _generate_pattern() -> void:
 	for x in width:
@@ -53,3 +56,22 @@ func _generate_pattern() -> void:
 			var rand_amount_of_presses: int = randi_range(0, modulo -1)
 			for i in rand_amount_of_presses:
 				_on_cell_pressed(Vector2i(x,y))
+
+
+func _cycle(index: Vector2i) -> void:
+	assert(index.x >= 0 && index.x < width && index.y >= 0 && index.y < height)
+	
+	var old_val = _cells[index.x][index.y].value
+	
+	if old_val == 0:
+		num_of_on_bits += 1
+	
+	_cells[index.x][index.y].value -= 1
+	
+	if _cells[index.x][index.y].value == 0:
+		num_of_on_bits -= 1
+
+func _set_num_of_on_bits(new_val: int) -> void:
+	num_of_on_bits = new_val
+	if num_of_on_bits == 0:
+		win.emit()
