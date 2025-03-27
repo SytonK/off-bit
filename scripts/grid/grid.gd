7a4075711ac020380ffe6ee9fdda0d0a3c76243d
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-27 19:07:00 +0200

    feat/on press cell set values

--- scripts/grid/grid.gd
+++ scripts/grid/grid.gd
@@ -28,6 +28,20 @@ func _init_grid() -> void:
 			var new_cell: Cell = CELL.instantiate()
 			new_cell.index = Vector2i(x, y)
 			new_cell.position = Vector2(x_position, y_position) * Cell.SIZE
+			new_cell.cell_pressed.connect(_on_cell_pressed)
+			add_child(new_cell)
 			
 			_cells[x][y] = new_cell
-			add_child(new_cell)
+
+func _on_cell_pressed(index: Vector2i) -> void:
+	assert(index.x >= 0 && index.x < width && index.y >= 0 && index.y < height)
+	
+	_cells[index.x][index.y].value -= 1
+	if index.x > 0:
+		_cells[index.x - 1][index.y].value -= 1
+	if index.x < width - 1:
+		_cells[index.x + 1][index.y].value -= 1
+	if index.y > 0:
+		_cells[index.x][index.y - 1].value -= 1
+	if index.y < height - 1:
+		_cells[index.x][index.y + 1].value -= 1
