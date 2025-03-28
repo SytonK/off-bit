09f0ac9b5ad9ac95362b90e433f923d0d5b60447
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-28 16:07:48 +0300

    feat/add mod to win screen

--- scripts/UI/menus/win menu/win_menu.gd
+++ scripts/UI/menus/win menu/win_menu.gd
@@ -7,4 +7,6 @@ func _ready() -> void:
 	visibility_changed.connect(_set_difficulty_label)
 
 func _set_difficulty_label() -> void:
-	difficuly_label.text = str(grid_metadata.width) + 'x' + str(grid_metadata.height)
+	var size_text: String = str(grid_metadata.width) + 'x' + str(grid_metadata.height)
+	var modulo_text: String = 'Mod: ' + str(grid_metadata.modulo) if grid_metadata.modulo > 2 else ''
+	difficuly_label.text = size_text + ', ' + modulo_text
