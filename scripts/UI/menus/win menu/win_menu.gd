c37ee1b20a8c54107d7a527ba56b51013a4c4f2d
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-03-28 17:26:07 +0300

    bug/win text

--- scripts/UI/menus/win menu/win_menu.gd
+++ scripts/UI/menus/win menu/win_menu.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 func _set_difficulty_label() -> void:
 	var size_text: String = str(grid_metadata.width) + 'x' + str(grid_metadata.height)
-	var modulo_text: String = 'Mod: ' + str(grid_metadata.modulo) if grid_metadata.modulo > 2 else ''
-	difficuly_label.text = size_text + ', ' + modulo_text
+	var modulo_text: String = ', Mod: ' + str(grid_metadata.modulo) if grid_metadata.modulo > 2 else ''
+	difficuly_label.text = size_text +  modulo_text
